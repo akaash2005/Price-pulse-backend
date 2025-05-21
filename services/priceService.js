e668ea092f59ab75a47a3ebe49c06f1398ed0c3c
akaash2005 <akaashvp@gmail.com>
2025-05-21 13:45:50 +0530

    Fix UUID import and install uuid package

--- services/priceService.js
+++ services/priceService.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 import { dbHelpers } from '../database.js';
 import { scrapeProductDetails } from './scraperService.js';
 
